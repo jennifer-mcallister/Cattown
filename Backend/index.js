4a3466bb12c4c07993615ecdcf83e22f643662ad
Jennifer McAllister <113523615+jennifer-mcallister@users.noreply.github.com>
2023-12-06 10:06:51 +0100

    set up frontend and backend envorinment

---
+++ Backend/index.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const userRoutes = require("./routes/userRoutes");
+const savefileRoutes = require("./routes/savefileRoutes");
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(`proccessing ${req.method} request to ${req.path}`);
+  next();
+});
+
+app.use("/api/v1/cattown/users", userRoutes);
+app.use("/api/v1/cattown/savefiles", savefileRoutes);
+
+
+const port = process.env.PORT || 5000;
+const run = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGODB);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+run();
