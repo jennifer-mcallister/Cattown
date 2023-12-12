8a523570aefb9d86002ab70349dc746dd557461d
Jennifer McAllister <113523615+jennifer-mcallister@users.noreply.github.com>
2023-12-12 11:43:37 +0100

    (build): add layout and navigation for header

---
+++ Cattown/src/components/styled/HeaderStyled.tsx
@@ -0,0 +1,24 @@
+import { styled } from "styled-components";
+
+export const HeaderGold = styled.div`
+  width: auto;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  height: 3rem;
+  background-color: lightgrey;
+
+  display: flex;
+  align-items: center;
+`;
+
+export const HeaderSmallContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  min-width: auto;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  gap: 2rem;
+  height: 100%;
+`;
