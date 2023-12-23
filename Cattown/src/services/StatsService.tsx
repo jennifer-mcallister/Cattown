bc489cc0d14db581f584d728e39676105dee66c3
Jennifer McAllister <113523615+jennifer-mcallister@users.noreply.github.com>
2023-12-23 15:33:45 +0100

    (feat): add updated user stats when buying relic

---
+++ Cattown/src/services/StatsService.tsx
@@ -0,0 +1,19 @@
+import { doc, updateDoc } from "firebase/firestore";
+import { IStats } from "../types/savefileTypes";
+import { auth, db } from "./Firebase";
+
+export const updateStats = async (stats: IStats) => {
+  try {
+    const loggedInUser = await auth.currentUser;
+
+    if (!loggedInUser) {
+      throw new Error("UnAuthorized");
+    }
+
+    const savefileRef = doc(db, "savefiles", loggedInUser.uid);
+    await updateDoc(savefileRef, { stats: stats });
+    console.log("Stats updated");
+  } catch {
+    throw new Error("503 Service Unavailable");
+  }
+};
